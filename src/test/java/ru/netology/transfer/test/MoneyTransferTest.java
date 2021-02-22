6e3d4fb688d3fc00886c95505efb89537b453736
Margarita Korolkova <mkorolkova311@gmail.com>
2021-02-22 23:39:06 +0300

    appveyor

--- src/test/java/ru/netology/transfer/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/transfer/test/MoneyTransferTest.java
@@ -22,7 +22,7 @@ public class MoneyTransferTest {
         val dashBoard = VerificationPage.validVerify(VerificationCode);
         val balance1 = dashBoard.getCardBalance(card1);
         val balance2 = dashBoard.getCardBalance(card2);
-        double addition = 5000.0;
+        double addition = 5000.50;
         dashBoard.additionToCard(card2, card1, addition);
         assertEquals(balance1-addition, dashBoard.getCardBalance(card1),0);
         assertEquals(balance2+addition, dashBoard.getCardBalance(card2),0);
@@ -54,7 +54,7 @@ public class MoneyTransferTest {
         val dashBoard = VerificationPage.validVerify(VerificationCode);
         double balance1 = dashBoard.getCardBalance(card1);
         double balance2 = dashBoard.getCardBalance(card2);
-        double addition = balance2 + 5000.0;
+        double addition = balance2 + 15000.0;
         dashBoard.additionToCard(card1, card2, addition);
         assertEquals(balance1+addition,dashBoard.getCardBalance(card1),0);
         assertEquals(balance2-addition,dashBoard.getCardBalance(card2),0);
